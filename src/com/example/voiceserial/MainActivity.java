9dcd61bbd9507ffd4322200ab5e4168ad6052faf
huangjoe <l_a@live.cn>
2015-07-27 17:10:25 +0800

    ID callback to VoiceToWord succed

--- src/com/example/voiceserial/MainActivity.java
+++ src/com/example/voiceserial/MainActivity.java
@@ -1,149 +1,141 @@
 package com.example.voiceserial;
 
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-import android.R.string;
+import zy.voice.MyRecognizerDialogLister.MyCallInterface;
+import zy.voice.VoiceToWord;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.app.Activity;
-import android.widget.EditText;
-import android.widget.SeekBar;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.app.Activity;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.widget.SeekBar;
-import android.widget.TextView;
-import android.widget.SeekBar.OnSeekBarChangeListener;
-import com.topeet.serialtest.*;
 import android.widget.Button;
 import android.widget.EditText;
-import zy.voice.*;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
+public class MainActivity extends Activity implements OnClickListener {
 
-public class MainActivity extends Activity implements OnClickListener{
-	Button but = null;
-	SeekBar sb;
-	//MySeekBar Msb;
-	private EditText content;
-	private int i=0;
-	private int ID;
-//	private boolean VoiceRecognizeFinishflag;
-//	SharedPreferences SeekBarProgess;
+   
+    Button but = null;
+    SeekBar sb;
+    // MySeekBar Msb;
+    private EditText content;
+    private final int i = 0;
+    private int mainID;
+    // private boolean VoiceRecognizeFinishflag;
+    // SharedPreferences SeekBarProgess;
 
-	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		
-		but = (Button) findViewById(R.id.button1);
-		but.setOnClickListener(this);
-        sb=(SeekBar)findViewById(R.id.seekBar01);
-        sb.setOnSeekBarChangeListener(sbLis);
-	//	com3.Open(3, 115200);
-	}
-	
-	@Override
-	public void onClick(View v) {
-//		SeekBarProgess = getSharedPreferences("ss", Context.MODE_PRIVATE);
-			//获取ss文件中User对应的数据，注意第二个参数，若此键值对中暂时没有数值，则默认返回第二个参数的值
-			//switch (v.getId()) {
-			//听写按钮
-			//case R.id.button1:
-				VoiceToWord voice = new VoiceToWord(MainActivity.this,"54ae8c54");//你申请的appid
-				voice.GetWordFromVoice();
-//				new senddata().sent(0);	
-//				ID = SeekBarProgess.getInt("ID",ID);
-//				sb.setProgress(ID);
-				//break;
-			
-			//}
-	}
-	
-	    	    	     
-	private OnSeekBarChangeListener sbLis=new OnSeekBarChangeListener(){
-		 
-		@Override
-		public void onProgressChanged(SeekBar seekBar, int progress,
-				boolean fromUser) {
-//			String str;
-			
-			new senddata().sent(progress);
-			//tv.setText(String.valueOf(sb.getProgress()));
-			
-		}
-		
-		@Override
-		public void onStartTrackingTouch(SeekBar seekBar) {
-			// 开始拖动时触发，与onProgressChanged区别在于onStartTrackingTouch在停止拖动前只触发一次
-			//而onProgressChanged只要在拖动，就会重复触发
-			//new senddata().sent(0);
-			//tv.setText(String.valueOf(sb.getProgress()));
-			
-		}
-		
-		  
-		
- 
-		@Override
-		public void onStopTrackingTouch(SeekBar seekBar) {
-			//结束拖动时触发
-			//new senddata().sent(0);
-			//tv.setText(String.valueOf(sb.getProgress()));
-			
-			
-		}
-    	
+    private final OnSeekBarChangeListener sbLis = new OnSeekBarChangeListener() {
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress,
+                boolean fromUser) {
+            // String str;
+
+            new senddata().sent(progress);
+            // tv.setText(String.valueOf(sb.getProgress()));
+
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+            // 开始拖动时触发，与onProgressChanged区别在于onStartTrackingTouch在停止拖动前只触发一次
+            // 而onProgressChanged只要在拖动，就会重复触发
+            // new senddata().sent(0);
+            // tv.setText(String.valueOf(sb.getProgress()));
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+            // 结束拖动时触发
+            // new senddata().sent(0);
+            // tv.setText(String.valueOf(sb.getProgress()));
+
+        }
+
     };
-	
-	@Override
-	protected void onResume() {
-		super.onResume();
-//		String str;
-//		int x=0;
-//		try {   
-//	        //获取logcat日志信息   
-//		Process mLogcatProc = null;   
-//		BufferedReader reader = null; 
-//	    mLogcatProc = Runtime.getRuntime().exec(new String[] { "logcat","Reutrn ID:E *:S" });   
-//	    reader = new BufferedReader(new InputStreamReader(mLogcatProc.getInputStream()));   
-//	   
-//	    String line;   
-//	      
-//	    while ((line = reader.readLine()) != null) {   
-//	          
-//	        if (line.length()> 0) {   
-//	        	 str=line.toString();
-//	        	x = Integer.parseInt(str);
-//	        }   
-//	    }   
-//	   
-//	} catch (Exception e) {   
-//	   
-//	    e.printStackTrace(); 
-//	    sb.setProgress(x);
-//	} 
-		//content.setText(((MyApplicaton)getApplication()).getText());
-	}
-	static{
+
+    static {
         System.loadLibrary("serialtest");
-}
-}
+    }
 
+    @Override
+    public void onClick(View v) {
+        // SeekBarProgess = getSharedPreferences("ss", Context.MODE_PRIVATE);
+        // 获取ss文件中User对应的数据，注意第二个参数，若此键值对中暂时没有数值，则默认返回第二个参数的值
+        // switch (v.getId()) {
+        // 听写按钮
+        // case R.id.button1:
 
+        Intent intent = new Intent(MainActivity.this,VoiceToWord.class);  
+
+        VoiceToWord voice = new VoiceToWord(MainActivity.this, "54ae8c54");// 你申请的appid
+        MainActivity.this.startActivityForResult(intent, 15);  
+        
+//        int debug=mainID;
+        // new senddata().sent(0);
+        // ID = SeekBarProgess.getInt("ID",ID);
+//         sb.setProgress(mainID);
+        // break;
+
+        // }
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {  
+        // TODO Auto-generated method stub  
+        super.onActivityResult(requestCode, resultCode, data);  
+        switch (requestCode) {// 通过requestCode来辨别数据来自哪个activity  
+        case 15:// 取得来自Activity2的数据，并显示于画面上  
+            String resultstr1 = data.getStringExtra("result");
+            String debugstr=resultstr1;
+            debugstr=resultstr1;
+            break;  
+        case 25:  
+        default:  
+            break;  
+        }  
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        but = (Button) findViewById(R.id.button1);
+        but.setOnClickListener(this);
+        sb = (SeekBar) findViewById(R.id.seekBar01);
+        sb.setOnSeekBarChangeListener(sbLis);
+        // com3.Open(3, 115200);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // String str;
+        // int x=0;
+        // try {
+        // //获取logcat日志信息
+        // Process mLogcatProc = null;
+        // BufferedReader reader = null;
+        // mLogcatProc = Runtime.getRuntime().exec(new String[] {
+        // "logcat","Reutrn ID:E *:S" });
+        // reader = new BufferedReader(new
+        // InputStreamReader(mLogcatProc.getInputStream()));
+        //
+        // String line;
+        //
+        // while ((line = reader.readLine()) != null) {
+        //
+        // if (line.length()> 0) {
+        // str=line.toString();
+        // x = Integer.parseInt(str);
+        // }
+        // }
+        //
+        // } catch (Exception e) {
+        //
+        // e.printStackTrace();
+        // sb.setProgress(x);
+        // }
+        // content.setText(((MyApplicaton)getApplication()).getText());
+    }
+
+   
+   }
--- src/zy/voice/VoiceToWord.java
+++ src/zy/voice/VoiceToWord.java
@@ -1,12 +1,16 @@
 package zy.voice;
 
+import zy.voice.MyRecognizerDialogLister.MyCallInterface;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Environment;
-import android.text.TextUtils;
+import android.widget.SeekBar;
 import android.widget.Toast;
+
+import com.example.voiceserial.MainActivity;
+import com.example.voiceserial.R;
 import com.iflytek.cloud.speech.SpeechConstant;
 import com.iflytek.cloud.speech.SpeechError;
 import com.iflytek.cloud.speech.SpeechListener;
@@ -16,193 +20,223 @@ import com.iflytek.cloud.ui.RecognizerDialog;
 import com.iflytek.cloud.ui.RecognizerDialogListener;
 import com.iflytek.sunflower.FlowerCollector;
 
+public class VoiceToWord extends Activity  implements MyCallInterface {
+    
+    public MyCallInterface mc;
+    private final Context context;
+    private final Toast mToast;
+    private int ID;
+    // 识别窗口
+    private RecognizerDialog iatDialog;
+    // 识别对象
 
-public class VoiceToWord extends Activity {
-	private Context context;
-	private Toast mToast;
-	private boolean ID;
-	// 识别窗口
-	private RecognizerDialog iatDialog;
-	// 识别对象
-	private SpeechRecognizer iatRecognizer;
-	// 缓存，保存当前的引擎参数到下一次启动应用程序使用.
-	private SharedPreferences mSharedPreferences;
-	private RecognizerDialogListener recognizerDialogListener = null;
+    final Intent intent = getIntent();  
+    private SpeechRecognizer iatRecognizer;
+    // 缓存，保存当前的引擎参数到下一次启动应用程序使用.
+    private final SharedPreferences mSharedPreferences;
 
-	public VoiceToWord(Context context, String APP_ID) {
-		// TODO Auto-generated constructor stub
-		// 用户登录
-		this.context = context;
-		// 初始化缓存对象.
-		mSharedPreferences = context.getSharedPreferences(
-				context.getPackageName(), MODE_PRIVATE);
-		SpeechUser.getUser().login(context, null, null, "appid=" + APP_ID,
-				listener);
-		// 初始化听写Dialog,如果只使用有UI听写功能,无需创建SpeechRecognizer
-		iatDialog = new RecognizerDialog(context);
-		mToast = Toast.makeText(context, "", Toast.LENGTH_LONG);
-		// 初始化听写Dialog,如果只使用有UI听写功能,无需创建SpeechRecognizer
-		iatDialog = new RecognizerDialog(context);
-		iatDialog.setCanceledOnTouchOutside(false);
+    private RecognizerDialogListener recognizerDialogListener = null;
 
-	}
+    /**
+     * 用户登录回调监听器.
+     */
+    private final SpeechListener listener = new SpeechListener() {
 
-	public VoiceToWord(Context context, String APP_ID,
-			RecognizerDialogListener recognizerDialogListener) {
-		this.context = context;
-		SpeechUser.getUser().login(context, null, null, "appid=" + APP_ID,
-				listener);
-		// 初始化听写Dialog,如果只使用有UI听写功能,无需创建SpeechRecognizer
-		iatDialog = new RecognizerDialog(context);
-		mToast = Toast.makeText(context, "", Toast.LENGTH_LONG);
-		// 初始化听写Dialog,如果只使用有UI听写功能,无需创建SpeechRecognizer
-		iatDialog = new RecognizerDialog(context);
-		// 在dialog外面不能取消
-		iatDialog.setCanceledOnTouchOutside(false);
-		// 初始化缓存对象.
-		mSharedPreferences = context.getSharedPreferences(
-				context.getPackageName(), MODE_PRIVATE);
-		this.recognizerDialogListener = recognizerDialogListener;
-	}
+        @Override
+        public void onCompleted(SpeechError error) {
+            if (error != null) {
+                System.out.println("user login success");
+            }
+        }
 
-	public void GetWordFromVoice() {
-		boolean isShowDialog = mSharedPreferences.getBoolean("iat_show", true);
-		if (isShowDialog) {
-			// 显示语音听写Dialog.
-			showIatDialog();
-		} else {
-			if (null == iatRecognizer) {
-				iatRecognizer = SpeechRecognizer.createRecognizer(this);
-           			// 设置返回结果格式
-//				iatRecognizer.setParameter(SpeechConstant.RESULT_TYPE, "json");
-//
-//				String lag = mSharedPreferences.getString(
-//						"iat_language_preference", "mandarin");
-//				if (lag.equals("en_us")) {
-//					// 设置语言
-//					iatRecognizer
-//							.setParameter(SpeechConstant.LANGUAGE, "en_us");
-//				} else {
-//					// 设置语言
-//					iatRecognizer
-//							.setParameter(SpeechConstant.LANGUAGE, "zh_cn");
-//					// 设置语言区域
-//					iatRecognizer.setParameter(SpeechConstant.ACCENT, lag);
-//				}
-//				// 设置语音前端点
-//				iatRecognizer.setParameter(SpeechConstant.VAD_BOS,
-//						mSharedPreferences.getString("iat_vadbos_preference",
-//								"4000"));
-//				// 设置语音后端点
-//				iatRecognizer.setParameter(SpeechConstant.VAD_EOS,
-//						mSharedPreferences.getString("iat_vadeos_preference",
-//								"1000"));
-//				// 设置标点符号
-//				iatRecognizer.setParameter(SpeechConstant.ASR_PTT,
-//						mSharedPreferences
-//								.getString("iat_punc_preference", "1"));
-//				// 设置音频保存路径
-//				iatRecognizer.setParameter(SpeechConstant.ASR_AUDIO_PATH,
-//						Environment.getExternalStorageDirectory()
-//								+ "/iflytek/wavaudio.pcm");//需在清单文件里添加sd卡的权限
-			}
-			if (iatRecognizer.isListening()) {
-				iatRecognizer.stopListening();
-				// ((Button)
-				// findViewById(android.R.id.button1)).setEnabled(false);
-			} else {
-			}
-		}
-		
-//		return ID;
-	}
+        @Override
+        public void onData(byte[] arg0) {
+        }
 
+        @Override
+        public void onEvent(int arg0, Bundle arg1) {
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
 
-	/**
-	 * 显示听写对话框.
-	 * 
-	 * @param
-	 */
-	
-	
-	public void showIatDialog() {
-		if (null == iatDialog) {
-			// 初始化听写Dialog
-			iatDialog = new RecognizerDialog(this);
-		}
+        VoiceToWord voice = new VoiceToWord(context, "54ae8c54");// 你申请的appid
+    }
+    public VoiceToWord(Context context, String APP_ID) {
+        // TODO Auto-generated constructor stub
+        // 用户登录
+        this.context = context;
+        // 初始化缓存对象.
+        mSharedPreferences = context.getSharedPreferences(
+                context.getPackageName(), MODE_PRIVATE);
+        SpeechUser.getUser().login(context, null, null, "appid=" + APP_ID,
+                listener);
+        // 初始化听写Dialog,如果只使用有UI听写功能,无需创建SpeechRecognizer
+        iatDialog = new RecognizerDialog(context);
+        mToast = Toast.makeText(context, "", Toast.LENGTH_LONG);
+        // 初始化听写Dialog,如果只使用有UI听写功能,无需创建SpeechRecognizer
+        iatDialog = new RecognizerDialog(context);
+        iatDialog.setCanceledOnTouchOutside(false);
 
-		// 获取引擎参数
-		String engine = mSharedPreferences.getString("iat_engine", "iat");
+    }
+    
 
-		// 清空Grammar_ID，防止识别后进行听写时Grammar_ID的干扰
-		iatDialog.setParameter(SpeechConstant.CLOUD_GRAMMAR, null);
-		// 设置听写Dialog的引擎
-		iatDialog.setParameter(SpeechConstant.DOMAIN, engine);
-		// 设置采样率参数，支持8K和16K
-		String rate = mSharedPreferences.getString("sf", "sf");
-		if (rate.equals("rate8k")) {
-			iatDialog.setParameter(SpeechConstant.SAMPLE_RATE, "8000");
-		} else {
-			iatDialog.setParameter(SpeechConstant.SAMPLE_RATE, "16000");
-		}
+    public VoiceToWord(Context context, String APP_ID,
+            RecognizerDialogListener recognizerDialogListener) {
+        this.context = context;
+        SpeechUser.getUser().login(context, null, null, "appid=" + APP_ID,
+                listener);
+        // 初始化听写Dialog,如果只使用有UI听写功能,无需创建SpeechRecognizer
+        iatDialog = new RecognizerDialog(context);
+        mToast = Toast.makeText(context, "", Toast.LENGTH_LONG);
+        // 初始化听写Dialog,如果只使用有UI听写功能,无需创建SpeechRecognizer
+        iatDialog = new RecognizerDialog(context);
+        // 在dialog外面不能取消
+        iatDialog.setCanceledOnTouchOutside(false);
+        // 初始化缓存对象.
+        mSharedPreferences = context.getSharedPreferences(
+                context.getPackageName(), MODE_PRIVATE);
+        this.recognizerDialogListener = recognizerDialogListener;
+    }
 
-		if (recognizerDialogListener == null) {
-			getRecognizerDialogListener();
-		}
+    public void CallbackID(int IDx) {
+        // TODO Auto-generated method stub
+        int debug ; 
+        ID = IDx;
+        String res = ID+"";  
+        intent.putExtra("result", res);// 把数据塞入intent里面  
+    }
+//    
+//   public void renewProgress(int ID)
+//   {
+//       int debug2 = ID;
+//       sb = (SeekBar) findViewById(R.id.seekBar01);
+//       sb.setProgress(ID);
+//    int debug3=sb.getProgress();
+//   }
+    private void getRecognizerDialogListener() {
+        /**
+         * 识别回调监听器
+         */
+        recognizerDialogListener = new MyRecognizerDialogLister(context);
+    }
 
-		// 显示听写对话框
-		iatDialog.setListener(recognizerDialogListener);
-		iatDialog.show();
-		 
-	}
+    public void GetWordFromVoice() {
+        boolean isShowDialog = mSharedPreferences.getBoolean("iat_show", true);
+        if (isShowDialog) {
+            // 显示语音听写Dialog.
+            showIatDialog();
+        } else {
+            if (null == iatRecognizer) {
+                iatRecognizer = SpeechRecognizer.createRecognizer(this);
+                // 设置返回结果格式
+                // iatRecognizer.setParameter(SpeechConstant.RESULT_TYPE,
+                // "json");
+                //
+                // String lag = mSharedPreferences.getString(
+                // "iat_language_preference", "mandarin");
+                // if (lag.equals("en_us")) {
+                // // 设置语言
+                // iatRecognizer
+                // .setParameter(SpeechConstant.LANGUAGE, "en_us");
+                // } else {
+                // // 设置语言
+                // iatRecognizer
+                // .setParameter(SpeechConstant.LANGUAGE, "zh_cn");
+                // // 设置语言区域
+                // iatRecognizer.setParameter(SpeechConstant.ACCENT, lag);
+                // }
+                // // 设置语音前端点
+                // iatRecognizer.setParameter(SpeechConstant.VAD_BOS,
+                // mSharedPreferences.getString("iat_vadbos_preference",
+                // "4000"));
+                // // 设置语音后端点
+                // iatRecognizer.setParameter(SpeechConstant.VAD_EOS,
+                // mSharedPreferences.getString("iat_vadeos_preference",
+                // "1000"));
+                // // 设置标点符号
+                // iatRecognizer.setParameter(SpeechConstant.ASR_PTT,
+                // mSharedPreferences
+                // .getString("iat_punc_preference", "1"));
+                // // 设置音频保存路径
+                // iatRecognizer.setParameter(SpeechConstant.ASR_AUDIO_PATH,
+                // Environment.getExternalStorageDirectory()
+                // + "/iflytek/wavaudio.pcm");//需在清单文件里添加sd卡的权限
+            }
+            if (iatRecognizer.isListening()) {
+                iatRecognizer.stopListening();
+                // ((Button)
+                // findViewById(android.R.id.button1)).setEnabled(false);
+            } else {
+            }
+        }
 
-	private void getRecognizerDialogListener() {
-		/**
-		 * 识别回调监听器
-		 */
-		recognizerDialogListener = new MyRecognizerDialogLister(context);
-	}
+//         return ID;
+    }
 
-	/**
-	 * 用户登录回调监听器.
-	 */
-	private SpeechListener listener = new SpeechListener() {
+    
+    public void onDestroy() {
+        // 退出时释放连接
+        iatRecognizer.cancel();
+        iatRecognizer.destroy();
+    };
 
-		@Override
-		public void onData(byte[] arg0) {
-		}
+    @Override
+    protected void onPause() {
+        // 移动数据统计分析
+        FlowerCollector.onPageEnd("VoiceToWord");
+        FlowerCollector.onPause(this);
+        super.onPause();
+    }
 
-		@Override
-		public void onCompleted(SpeechError error) {
-			if (error != null) {
-				System.out.println("user login success");
-			}
-		}
+    @Override
+    protected void onResume() {
+        // 移动数据统计分析
+        FlowerCollector.onResume(this);
+        FlowerCollector.onPageStart("VoiceToWord");
+        super.onResume();
+    }
 
-		@Override
-		public void onEvent(int arg0, Bundle arg1) {
-		}
-	};
+    /**
+     * 显示听写对话框.
+     * 
+     * @param
+     */
 
-	protected void onDestroy() {
-		// 退出时释放连接
-		iatRecognizer.cancel();
-		iatRecognizer.destroy();
-	};
+    public void showIatDialog() {
+        if (null == iatDialog) {
+            // 初始化听写Dialog
+            iatDialog = new RecognizerDialog(this);
+        }
 
-	@Override
-	protected void onResume() {
-		// 移动数据统计分析
-		FlowerCollector.onResume(this);
-		FlowerCollector.onPageStart("VoiceToWord");
-		super.onResume();
-	}
+        // 获取引擎参数
+        String engine = mSharedPreferences.getString("iat_engine", "iat");
 
-	@Override
-	protected void onPause() {
-		// 移动数据统计分析
-		FlowerCollector.onPageEnd("VoiceToWord");
-		FlowerCollector.onPause(this);
-		super.onPause();
-	}
+        // 清空Grammar_ID，防止识别后进行听写时Grammar_ID的干扰
+        iatDialog.setParameter(SpeechConstant.CLOUD_GRAMMAR, null);
+        // 设置听写Dialog的引擎
+        iatDialog.setParameter(SpeechConstant.DOMAIN, engine);
+        // 设置采样率参数，支持8K和16K
+        String rate = mSharedPreferences.getString("sf", "sf");
+        if (rate.equals("rate8k")) {
+            iatDialog.setParameter(SpeechConstant.SAMPLE_RATE, "8000");
+        } else {
+            iatDialog.setParameter(SpeechConstant.SAMPLE_RATE, "16000");
+        }
+
+        if (recognizerDialogListener == null) {
+            getRecognizerDialogListener();
+        }
+
+        // 显示听写对话框
+        iatDialog.setListener(recognizerDialogListener);
+        iatDialog.show();
+
+    }
+
+    @Override
+    public void setCallfuc(MyCallInterface mc) {
+        // TODO Auto-generated method stub
+        this.mc=mc;
+    }
 }
